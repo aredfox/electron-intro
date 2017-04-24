bb705dbe86079f949aa46b7aa4c2626e07660268
yves.schelpe@kdg.be <yves.schelpe@kdg.be>
2017-04-24 14:26:26 +0200

    toggle icon

--- 03-integration/main.js
+++ 03-integration/main.js
@@ -1,19 +1,24 @@
-const { electron, app, BrowserWindow, Tray } = require('electron');
+const { electron, app, BrowserWindow, Tray, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
 const os = require('os');
 const imageFolder = path.join(__dirname, '/resources/');
 const images = {
-    'red': path.join(imageFolder, `red${os.platform() === 'darwin' ? '-16.png' : '.ico'}`)
+    'red': path.join(imageFolder, `red${os.platform() === 'darwin' ? '-16.png' : '.ico'}`),
+    'green': path.join(imageFolder, `green${os.platform() === 'darwin' ? '-16.png' : '.ico'}`),
+    'blue': path.join(imageFolder, `blue${os.platform() === 'darwin' ? '-16.png' : '.ico'}`)
 };
+
 
 let mainWindow;
 let tray;
+let trayImage = images.red;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 775, height: 600,
-        resizable: false
+        resizable: false,
+        icon: images.blue
     });
 
     mainWindow.loadURL(
@@ -31,7 +36,7 @@ function createWindow() {
     createTrayIcon();
 }
 function createTrayIcon() {
-    tray = new Tray(images.red);
+    tray = new Tray(trayImage);
 }
 
 function handleActivate() {
@@ -52,3 +57,18 @@ app.on('will-quit', handleQuit);
 app.on('ready', createWindow);
 app.on('activate', handleActivate);
 app.on('window-all-closed', handleAllWindowsClosed);
+
+ipcMain.on('toggle', (event, includeAppIcon) => {
+    console.log(`Current tray image: ${trayImage}`);
+    trayImage === images.red
+        ? trayImage = images.green
+        : trayImage = images.red;
+    tray.setImage(trayImage);
+    tray.setTitle(trayImage);
+
+    if (includeAppIcon) {
+        mainWindow.setIcon(trayImage);
+    } else {
+        mainWindow.setIcon(images.blue);
+    }
+});
